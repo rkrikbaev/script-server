09a9d84adc10c541f4b5bea55bf23ea54579f54e
rkrikbaev <r.krikbaev@zeinetsse.com>
2018-10-23 04:34:32 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get upgrade -y
+
+
+PACKAGES=$(xargs <<EOF
+build-essential
+curl
+git
+python3-dev
+python3-setuptools
+python3-tk
+python3-wheel
+python-psycopg2
+locales
+gfortran
+swig
+libagg-dev
+libatlas-dev
+libffi-dev
+libfreetype6-dev
+liblapack-dev
+libncurses5-dev
+libopenblas-dev
+libpng12-dev
+libpq-dev
+libxft-dev
+libxml2-dev
+libxslt-dev
+zlib1g-dev
+ttf-bitstream-vera
+EOF
+)
+
+apt-get install -y $PACKAGES
+apt-get clean
